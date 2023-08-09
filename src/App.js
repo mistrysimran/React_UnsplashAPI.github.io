4132fd166b1c10e4bfb8f5dfddc2defe434297cd
mistrysimran <mistrysimran31@gmail.com>
2023-08-09 21:59:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [results, setResult] = useState([]);
+  const [value, setValue] = useState("");
+
+  const fetchImages = (event) => {
+
+    event.preventDefault();
+    // setValue(e.target.value)
+    console.log("hii");
+    console.log(value);
+    fetch(`https://api.unsplash.com/search/photos?client_id=-wMfwmU-NULeey7ucsQ821_Cr0cA5lx-WzR90WEN7do&query=${value}&orientation=squarish&per_page=12`)
+      .then(res => res.json())
+      .then(data => {
+        console.log("here", data.results)
+        setResult(data.results)
+      }
+      );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="header mb-3">
+        {/* <h1>PicSearch</h1> */}
+
+        <form onSubmit={fetchImages}>
+          <div className="inp mt-3">
+            <span>PicSearch</span>
+            <div className='d-flex sbox'>
+              <span>Search</span>
+              <input type="text" className=""
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Browse Images.." />
+              <button className="" type="submit" ><i className="fa fa-search"></i></button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="container-fluid mt-2 mb-3">
+        <div className="row g-3">
+          {results.map((image) => (
+            <div className="col-md-3" key={image.id}>
+              <img src={image.urls.regular} alt="" className="img-fluid" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </>
   );
 }
 
